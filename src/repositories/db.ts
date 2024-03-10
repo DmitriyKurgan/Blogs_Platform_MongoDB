5637730e7f234106303e82c777ce709b3e55f608
Dmitry Kurgan <dimakurgan123789@gmail.com>
2024-03-10 14:46:58 +0200

    Changes

--- src/repositories/db.ts
+++ src/repositories/db.ts
@@ -1,4 +1,7 @@
 import { MongoClient } from 'mongodb';
+import dotenv from 'dotenv'
+dotenv.config()
+
 //|| 'mongodb+srv://dimakurgan123789:annogolik123789@blogsplatform.mxifx0s.mongodb.net/?retryWrites=true&w=majority&appName=BlogsPlatform';
 const mongoURI = process.env.MONGO_URL
 if (!mongoURI){
