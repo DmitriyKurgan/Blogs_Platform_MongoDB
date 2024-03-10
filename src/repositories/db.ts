e01332d23022742191003161e3d40b50267c3583
Dmitry Kurgan <dimakurgan123789@gmail.com>
2024-03-10 14:45:20 +0200

    Changes

--- src/repositories/db.ts
+++ src/repositories/db.ts
@@ -1,10 +1,9 @@
 import { MongoClient } from 'mongodb';
-
-
-//const mongoURI = process.env.MONGO_URL || "mongodb://0.0.0.0:27017:/?maxPoolSize=206w=majority";
-//MONGO_URL="mongodb+srv://a:a@ava.epzello.mongodb.net/?retryWrites=true&w=majority"
-const mongoURI = process.env.MONGO_URL || 'mongodb+srv://dimakurgan123789:annogolik123789@blogsplatform.mxifx0s.mongodb.net/?retryWrites=true&w=majority&appName=BlogsPlatform';
-
+//|| 'mongodb+srv://dimakurgan123789:annogolik123789@blogsplatform.mxifx0s.mongodb.net/?retryWrites=true&w=majority&appName=BlogsPlatform';
+const mongoURI = process.env.MONGO_URL
+if (!mongoURI){
+    throw new Error('Database url is not defined!')
+}
 export const client = new MongoClient(mongoURI);
 
 export async function runDB (){
